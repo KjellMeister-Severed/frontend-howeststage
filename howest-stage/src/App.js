4ac49724950b25d589447dd299df9f2638816697
Kjell Maekelberg <maekelbergkjell@gmail.com>
2021-11-09 15:58:03 +0100

    Implement different routing method for copanies

--- howest-stage/src/App.js
+++ howest-stage/src/App.js
@@ -1,17 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-import  Home  from './components/Home'
-import { Routes, Route, Link } from "react-router-dom";
-import {useState, useEffect} from "react";
-import About from "./components/About";
+import  Home  from './components/Home/Home'
+import { Routes, Route } from "react-router-dom";
+import About from "./components/about/About";
+import Dashboard from "./components/dashboard/dashboard";
+import StudentDashboard from "./components/dashboard/student/StudentDashboard";
+import CompanyDashboard from "./components/dashboard/CompanyDashboard";
+
+
+const Test = () => {
+    return (
+        <div>
+            hallo
+        </div>
+    )
+}
 
 function App() {
   return (
     <div className={"App"}>
       <h1>Dit is de root</h1>
       <Routes>
-          <Route path={"/"} element={<Home />} />
-          <Route path={"about"} element={<About />}/>
+          <Route path={"/"} element={<StudentDashboard />} />
+          <Route path={"/dashboard/student"} element={<StudentDashboard />} />
+          <Route path={"/dashboard/company"} element={<CompanyDashboard />} />
       </Routes>
     </div>
   );
--- howest-stage/src/index.js
+++ howest-stage/src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Home from "./components/Home";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
