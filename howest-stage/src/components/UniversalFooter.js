26e40cdc24f8725eebb4f5046feda88c78972e6b
Kjell Maekelberg <maekelbergkjell@gmail.com>
2021-11-10 17:31:13 +0100

    fix developer cycle

--- howest-stage/src/components/UniversalFooter.js
+++ howest-stage/src/components/UniversalFooter.js
@@ -6,9 +6,28 @@ class UniversalFooter extends React.Component {
         this.contributors = [
             `<li className={"mr-2"}><a className={"hover:text-magenta text-white"} href="https://www.linkedin.com/in/kjell-maekelberg/">Kjell Maekelberg</a></li>`,
             `<li className={"mr-2"}><a className={"hover:text-magenta text-white"} href="https://www.linkedin.com/in/borobbrecht/">Bo Robbrecht</a></li>`,
-            `<li className={"mr-2"}><a className={"hover:text-magenta text-white"} href="https://www.linkedin.com/in/adelina-giurea-05b352221/">Adelina Giurea</a></li>`,
+            ``,
             `<li className={"mr-2"}><a className={"hover:text-magenta text-white"} href="#">Adriaan De Saeger</a></li>`
         ]
+        this.contributors = [
+            {
+                linkedInURL: "https://www.linkedin.com/in/kjell-maekelberg/",
+                name: "Kjell Maekelberg"
+            },
+            {
+                linkedInURL: "https://www.linkedin.com/in/borobbrecht/",
+                name: "Bo Robbrecht"
+            },
+            {
+                linkedInURL: "https://www.linkedin.com/in/adelina-giurea-05b352221/",
+                name: "Adelina Giurea"
+            },
+            {
+                linkedInURL: "https://www.linkedin.com/in/kjell-maekelberg/",
+                name: "Adriaan De Saeger"
+            }
+        ]
+
         this.state = {
             count: 0,
             contributor: this.contributors[0]
@@ -17,7 +36,7 @@ class UniversalFooter extends React.Component {
 
     componentDidMount() {
         this.timerID = setInterval(() => {
-                if (this.state.count <= this.contributors.length) {
+                if (this.state.count < this.contributors.length - 1) {
                     this.setState((state,props) => ({
                         count: state.count + 1
                     }))
@@ -44,11 +63,13 @@ class UniversalFooter extends React.Component {
     }
 
     render() {
+        console.log(this.state.count)
+        console.log(this.contributors.length)
         return (
             <footer
                 className={`${this.props.className} flex bottom-0 justify-between items-center fixed bg-primary p-4`}>
                 <ul className={"flex w-3/5"}>
-                    {this.state.contributor}
+                    <li className={"mr-2"}><a className={"hover:text-magenta text-white"} href={this.state.contributor.linkedInURL}>{this.state.contributor.name}</a></li>
                 </ul>
                 <a href="#" className={"text-white font-bold underline"}>Legal Stuff</a>
             </footer>
