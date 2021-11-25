9e4a060734fd8961cd2d3b87d70b313c2d6f7492
Bo <bo.robbrecht@student.howest.be>
2021-11-25 14:51:51 +0100

    Finished implementing showing LookingFor... finally...

--- howest-stage/src/components/Student/CompanyInfo.js
+++ howest-stage/src/components/Student/CompanyInfo.js
@@ -1,40 +1,56 @@
-import { Component } from 'react';
+import {useEffect, useState} from 'react';
 
-class CompanyInfo extends Component {
-    render() {
-        return (
-            <article className={"mx-auto w-fit p-4 flex flex-col items-center"}>
-                <h2 className={"font-vagbold text-2xl mb-4"}>{this.props.name}</h2>
-                <div className={"flex flex-row gap-4"}>
-                    <SubInfo heading="Address">
-                        <p>{this.props.street}</p>
-                        <p>{this.props.postalCode} {this.props.city}</p>
-                    </SubInfo>
-                    <SubInfo heading="Contact">
-                        <p>{this.props.email}</p>
-                        <p>{this.props.website}</p>
-                    </SubInfo>
-                    <SubInfo heading="Looking for">
-                        <p>{this.props.lookingFor.replace("\n", " ")}</p>
-                    </SubInfo>
-                </div>
-                <div>
-                    {this.props.description}
-                </div>
-            </article>
-        );
+export default function CompanyInfo(props){
+    let [company, setCompany] = useState('')
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_BACK_END_ENDPOINT}:${process.env.REACT_APP_BACK_END_PORT}/api/companies/${props.id}`)
+            .then(data => data.json())
+            .then(json => {
+                json.lookingfor = json.lookingfor.split(/\r?\n/)
+                setCompany(json)
+            })
+    }, [props.id])
+
+    function iterateLF(){
+        let i = 0
+        return company.lookingfor.map(item => {
+            let res = (<p key={i}>{item}</p>)
+            i = i + 1
+            return res
+        })
     }
+
+    return(
+        <article className={"mx-auto w-fit p-4 flex flex-col items-center"}>
+            <h2 className={"font-vagbold text-2xl mb-4"}>{company.name}</h2>
+            <div className={"flex flex-row gap-4"}>
+                <SubInfo heading="Address">
+                    <p>{company.street}</p>
+                    <p>{company.postalCode} {company.city}</p>
+                </SubInfo>
+                <SubInfo heading="Contact">
+                    <p>{company.email}</p>
+                    <p>{company.website}</p>
+                </SubInfo>
+                <SubInfo heading="Looking for">
+                    {
+                        iterateLF()
+                    }
+                </SubInfo>
+            </div>
+            <div>
+                {company.description}
+            </div>
+        </article>
+    )
 }
 
-class SubInfo extends Component {
-    render() {
-        return (
-            <section className={"flex flex-col items-center"}>
-                <h3 className={"font-vag underline text-xl"}>{this.props.heading}</h3>
-                {this.props.children}
-            </section>
-        );
-    }
+function SubInfo(props) {
+    return (
+        <section className={"flex flex-col items-center"}>
+            <h3 className={"font-vag underline text-xl"}>{props.heading}</h3>
+            {props.children}
+        </section>
+    );
 }
-
-export default CompanyInfo;
--- howest-stage/src/pages/student/CompanyInfo.js
+++ howest-stage/src/pages/student/CompanyInfo.js
@@ -2,20 +2,12 @@ import UniversalHeader from '../../components/UniversalHeader';
 import MediumButton from '../../components/MediumButton';
 import UniversalFooter from '../../components/UniversalFooter';
 import { useParams } from 'react-router';
-import { useEffect, useState } from 'react';
 import CompanyInfo from '../../components/Student/CompanyInfo';
 
 
 
 export default function StudentCompanyInfo() {
     const { id } = useParams()
-    let [company, setCompany] = useState('')
-    
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_BACK_END_ENDPOINT}:${process.env.REACT_APP_BACK_END_PORT}/api/companies/${id}`)
-            .then(data => data.json())
-            .then(json => setCompany(json))
-    }, [id])
 
     return (
         <>
@@ -31,19 +23,7 @@ export default function StudentCompanyInfo() {
                 <MediumButton className={"border-2 border-white rounded bg-magenta mr-5"} textColor={"text-white"}>Logout (todo)</MediumButton>
             </UniversalHeader>
             <main className={"pt-20" }>
-                { console.log(company)}
-                <CompanyInfo
-                    name={company.name}
-                    street={company.address}
-                    postalCode={company.postalcode}
-                    city={company.city}
-                    email={company.email}
-                    website={company.website}
-                    lookingFor={company.lookingfor}
-                >
-                    { company.description}
-                </CompanyInfo>
-                
+                <CompanyInfo id={id}/>
             </main>
             <UniversalFooter className={"h-20 w-screen"} />
         </>
