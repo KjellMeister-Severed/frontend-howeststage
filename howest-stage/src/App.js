3c272eb228cc7822edb9d6bb8697d527bd605f5a
Bo <bo.robbrecht@student.howest.be>
2021-12-16 14:45:51 +0100

    Added error handling (but doubles messafe for unknown reason

--- howest-stage/src/App.js
+++ howest-stage/src/App.js
@@ -7,19 +7,22 @@ import CompanyDashboard from './pages/company/Dashboard';
 import Home from './pages/Home';
 import Legal from "./pages/legal/Legal";
 import StudentCompanyInfo from './pages/student/CompanyInfo';
+import React from 'react';
 
 function App() {
     return (
         <MsalProvider instance={msalInstance}>
-            <div className={"App"}>
-                <Routes>
-                    <Route path={"/"} element={<Home />} />
-                    <Route path={"/legal"} element={<Legal />} />
-                    <Route path={"/dashboard/student"} element={<StudentDashboard />} />
-                    <Route path={"/dashboard/company"} element={<CompanyDashboard />} />
-                    <Route path={"/company/:id/info"} element={<StudentCompanyInfo />} component={StudentCompanyInfo} />
-                </Routes>
-            </div>
+            <React.StrictMode>
+                <div className={"App"}>
+                    <Routes>
+                        <Route path={"/"} element={<Home />} />
+                        <Route path={"/legal"} element={<Legal />} />
+                        <Route path={"/dashboard/student"} element={<StudentDashboard />} />
+                        <Route path={"/dashboard/company"} element={<CompanyDashboard />} />
+                        <Route path={"/company/:id/info"} element={<StudentCompanyInfo />} component={StudentCompanyInfo} />
+                    </Routes>
+                </div>
+            </React.StrictMode>
         </MsalProvider>
     );
 }
--- howest-stage/src/components/Company/Signin.js
+++ howest-stage/src/components/Company/Signin.js
@@ -1,15 +1,67 @@
-function CompanySignIn() {
-    return ( 
-        <section className={"flex flex-col items-center m-2 lg:flex-col gap-2 lg:gap-0.5"}>
-            <h3 className={"font-bold"}>Welcome back</h3>
-            <p>Just enter you email and we'll send you a sign-in link</p>
-            <form>
-                <label for={"email"}>Email: </label>
-                <input type={"email"} name={"email"} placeholder={"yourname@youremail.com"} className={"border-solid border-2 p-1"} />
-                <input type={"submit"} value={"Submit"} className={"p-2 rounded bg-magenta hover:bg-white hover:text-black text-white ml-2"}/>
-            </form>
-        </section>
-    );
+import { fetchFromBackend } from "../Comms";
+import { Component } from "react";
+
+class CompanySignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            errorbag: []
+        };
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.generateMagicLink = this.generateMagicLink.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({ email: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+        this.setState((state) => state.errorbag, this.generateMagicLink)
+        
+        
+    }
+
+    generateMagicLink() { 
+        fetchFromBackend("/api/companies", "GET").then(data => {
+            const results = data.filter(company => company.email === this.state.email)
+            if (results.length === 0) {
+                this.setState((state) => {
+                    state.errorbag.push("No account was found with the given email")
+                })
+            }
+        })
+    }
+
+    render() {
+        return (
+            <section className={"flex flex-col items-center m-2 lg:flex-col gap-2 lg:gap-0.5"}>
+                <aside>
+                    {this.state.errorbag.length > 0 &&
+                        <ul>
+                            {this.state.errorbag.map(error => <li className="list-disc">{error}</li>)}
+                        </ul>
+                    }
+                </aside>
+                <p>Just enter you email and we'll send you a sign-in link</p>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Email: </label>
+                    <input
+                        type={"email"}
+                        name={"email"}
+                        id={"email"}
+                        value={this.state.email}
+                        placeholder={"yourname@youremail.com"}
+                        className={"border-solid border-2 p-1"}
+                        onChange={this.handleChange}
+                    />
+                    <input type={"submit"} value={"Submit"} className={"p-2 rounded bg-magenta hover:bg-white hover:text-black text-white ml-2"} />
+                </form>
+            </section >
+        );
+    }
 }
 
 export default CompanySignIn;
