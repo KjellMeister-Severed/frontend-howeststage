736fbb290a7d78f97572a8c4890ac397012d953e
Bo <bo.robbrecht@student.howest.be>
2021-12-16 11:56:20 +0100

    Added form for company sign-in

--- howest-stage/src/components/Company/Signin.js
+++ howest-stage/src/components/Company/Signin.js
@@ -1,9 +1,13 @@
-import UniversalHeader from "../UniversalHeader";
-
 function CompanySignIn() {
     return ( 
-        <>
-        </>
+        <section className={"flex flex-col items-center m-2 lg:flex-col gap-2 lg:gap-0.5"}>
+            <h3 className={"font-bold"}>Welcome back</h3>
+            <p>Just enter you email and we'll send you a sign-in link</p>
+            <form>
+                <label for={"email"}>Email: </label>
+                <input type={"email"} name={"email"} placeholder={"yourname@youremail.com"} className={"border-solid border-2 p-1"}/>
+            </form>
+        </section>
     );
 }
 
