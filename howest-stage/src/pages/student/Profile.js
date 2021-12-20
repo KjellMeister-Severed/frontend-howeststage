46169d4931d1909ed73d7a3ddd14e2a02e45d654
Bo <bo.robbrecht@student.howest.be>
2021-12-20 16:18:19 +0100

    Abstracted HTML components

--- howest-stage/src/pages/student/Profile.js
+++ howest-stage/src/pages/student/Profile.js
@@ -3,6 +3,7 @@ import { Component } from "react";
 import UniversalHeader from "../../components/UniversalHeader";
 import MediumButton from "../../components/MediumButton";
 import { fetchFromBackend } from "../../components/Comms";
+import HeroBanner from "../../components/Student/HeroBanner";
 
 export default class StudentProfile extends Component {
     static contextType = MsalContext
@@ -30,9 +31,10 @@ export default class StudentProfile extends Component {
             }))
             fetchFromBackend("/api/user/", "GET", response.accessToken)
                 .then(data => { 
+                    console.log(data)
                     this.setState((state) => ({
                         cv: data.cv_path,
-                        linkedin: data.linkedin_path
+                        linkedin: data.linkedin_url
                     }))
                 })
         }).catch(data => console.log(data))
@@ -53,35 +55,28 @@ export default class StudentProfile extends Component {
                     <MediumButton className={"border-2 border-white rounded bg-magenta mr-5"} onClick={() => this.context.instance.logoutRedirect()} textColor={"text-white"}>Logout</MediumButton>
                 </UniversalHeader>
                 <main className="pt-24 gap-2 mt-18 mx-5 font-vag">
-                    <div className={"flex flex-row justify-center gap-10 flex-wrap"}>
-                        <img src="https://via.placeholder.com/300" alt={this.state.name} className={"h-72 w-auto"}/>
-                        <aside className="flex flex-col w-max justify-around">
-                            <h2 className={"text-xl font-vagbold"}>{this.state.name}</h2>
-                            <p><span className={"underline"}>Email:</span> {this.state.email}</p>
-                            <p><span className={"underline"}>Linkedin:</span> {
-                                (this.state.linkedin)
-                                    ? <MediumButton
-                                        to={this.state.linkedin}
-                                        className={"hover:text-black hover:border-black border border-solid"}
-                                        bg={"bg-teal"}
-                                        bgHover={"bg-white"}
-                                        textColor={"text-white"}>Available</MediumButton>
-                                    : <span className={"text-red-600"}>Not yet set!</span>
-                            }</p>
-                            <p><span className={"underline mr-3"}>CV:</span> {
-                                (this.state.cv)
-                                    ? <MediumButton
-                                        to={this.state.cv}
-                                        className={"hover:text-black hover:border-black border border-solid"}
-                                        bg={"bg-teal"}
-                                        bgHover={"bg-white"}
-                                        textColor={"text-white"}>Available</MediumButton>
-                                    : <span className={"text-red-600"}>Not yet uploaded!</span>
-                            }</p>
-                        </aside>
-                    </div>
+                    <HeroBanner name={this.state.name}>
+                        <p><span className={"underline"}>Email:</span>{this.state.email}</p>
+                        <DisplayEntry title="CV" entry={this.state.cv} missingText="Not yet uploaded" />
+                        <DisplayEntry title="LinkedIn" entry={this.state.linkedin} missingText="Not yet set"/>
+                    </HeroBanner>
                 </main>
             </>
         )
     }
+}
+
+function DisplayEntry(props) {
+    return (
+        <p><span className={"underline mr-3"}>{props.title}:</span> {
+            (props.entry)
+                ? <MediumButton
+                    to={props.entry}
+                    className={"hover:text-black hover:border-black border border-solid"}
+                    bg={"bg-teal"}
+                    bgHover={"bg-white"}
+                    textColor={"text-white"}>Available!</MediumButton>
+                : <span className={"text-red-600"}>{props.missingText}</span>
+        }</p>
+    );
 }
