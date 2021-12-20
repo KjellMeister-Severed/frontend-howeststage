19d8d0d7c1c9ffd9c767f4f660d20b491ef88101
Bo <bo.robbrecht@student.howest.be>
2021-12-20 16:02:58 +0100

    Styled Hero section

--- howest-stage/src/pages/student/Profile.js
+++ howest-stage/src/pages/student/Profile.js
@@ -13,7 +13,8 @@ export default class StudentProfile extends Component {
             name: '',
             email: '',
             cv: '',
-            linkedin: ''
+            linkedin: '',
+            errorbag: []
         }
     }
 
@@ -51,11 +52,34 @@ export default class StudentProfile extends Component {
                     </MediumButton>
                     <MediumButton className={"border-2 border-white rounded bg-magenta mr-5"} onClick={() => this.context.instance.logoutRedirect()} textColor={"text-white"}>Logout</MediumButton>
                 </UniversalHeader>
-                <main className="flex flex-row pt-24 gap-2 mt-18 mx-5">
-                    <figure>
-                        <img src="https://via.placeholder.com/300" alt={this.state.name}/>
-                    </figure>
-                    <h2 className={"text-xl font-vagbold"}>{this.state.name}</h2>
+                <main className="pt-24 gap-2 mt-18 mx-5 font-vag">
+                    <div className={"flex flex-row justify-center gap-10 flex-wrap"}>
+                        <img src="https://via.placeholder.com/300" alt={this.state.name} className={"h-72 w-auto"}/>
+                        <aside className="flex flex-col w-max justify-around">
+                            <h2 className={"text-xl font-vagbold"}>{this.state.name}</h2>
+                            <p><span className={"underline"}>Email:</span> {this.state.email}</p>
+                            <p><span className={"underline"}>Linkedin:</span> {
+                                (this.state.linkedin)
+                                    ? <MediumButton
+                                        to={this.state.linkedin}
+                                        className={"hover:text-black hover:border-black border border-solid"}
+                                        bg={"bg-teal"}
+                                        bgHover={"bg-white"}
+                                        textColor={"text-white"}>Available</MediumButton>
+                                    : <span className={"text-red-600"}>Not yet set!</span>
+                            }</p>
+                            <p><span className={"underline mr-3"}>CV:</span> {
+                                (this.state.cv)
+                                    ? <MediumButton
+                                        to={this.state.cv}
+                                        className={"hover:text-black hover:border-black border border-solid"}
+                                        bg={"bg-teal"}
+                                        bgHover={"bg-white"}
+                                        textColor={"text-white"}>Available</MediumButton>
+                                    : <span className={"text-red-600"}>Not yet uploaded!</span>
+                            }</p>
+                        </aside>
+                    </div>
                 </main>
             </>
         )
